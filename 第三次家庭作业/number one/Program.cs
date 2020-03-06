24c78f513f765eaffafd523f9d712f0d9fb9e3a1
ysx1024 <2650974406@qq.com>
2020-03-06 11:09:54 +0800

    第三次家庭作业

---
+++ 第三次家庭作业/number one/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace number_one
+{
+    interface Shape
+    {
+        void Legitimate();
+        void Area();
+    }
+    class Rectangle:Shape
+    {
+        public double length;
+        public double width;
+        private bool legitimate;
+        public Rectangle(double m,double n)
+        {
+            length = m;
+            width = n;
+        }
+        public void Legitimate()
+        {
+            if (length == width || length <= 0 || width <= 0)
+                legitimate = false;
+            else
+                legitimate = true;
+        }
+        public void Area()
+        {
+            if (legitimate)
+            {
+                double area = length * width;
+                Console.WriteLine("该长方形合法，面积为" + area);
+            }
+            else
+                Console.WriteLine("该长方形不合法");
+        }
+    }
+    class Square:Shape
+    {
+        public double side;
+        private bool legitimate;
+
+        public Square(double x)
+        {
+            side = x;
+        }
+        public void Legitimate()
+        {
+            if (side <= 0)
+                legitimate = false;
+            else
+                legitimate = true;
+        }
+        public void Area()
+        {
+            if (legitimate)
+            {
+                double area = side*side;
+                Console.WriteLine("该正方形合法，面积为" + area);
+            }
+            else
+                Console.WriteLine("该正方形不合法");
+        }
+    }
+    class Triangle:Shape
+    {
+        public double a, b, c;
+        private bool legitimate;
+
+        public Triangle(double x,double y,double z)
+        {
+            a = x;
+            b = y;
+            c = z;
+        }
+        public void Legitimate()
+        {
+            if ((a + b) <= c || a <= 0 || b <= 0 || c <= 0)
+                legitimate = false;
+            else
+                legitimate = true;
+        }
+        public void Area()
+        {
+            if (legitimate)
+            {
+                double p = (a + b + c) / 2;
+                double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                Console.WriteLine("该三角形合法，面积为" + area);
+            }
+            else
+                Console.WriteLine("该三角形不合法");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(13,14);
+            rectangle.Legitimate();
+            rectangle.Area();
+
+            Square square = new Square(10);
+            square.Legitimate();
+            square.Area();
+
+            Triangle triangle = new Triangle(3,4,5);
+            triangle.Legitimate();
+            triangle.Area();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 第三次家庭作业/number two/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace number_two
+{
+    interface Shape
+    {
+        double Area();
+        bool Legitimate();
+    }
+    class Rectangle : Shape
+    {
+        public double length;
+        public double width;
+        private bool legitimate;
+        public Rectangle(double m, double n)
+        {
+            length = m;
+            width = n;
+        }
+        public bool Legitimate()
+        {
+            if (length > 0 && width > 0)
+                return legitimate = true;
+            else
+                return legitimate = false;
+        }
+        public double Area()
+        {
+                double area = length * width;
+            return area;
+        }
+    }
+    class Square : Shape
+    {
+        public double side;
+        private bool legitimate;
+
+        public Square(double x)
+        {
+            side = x;
+        }
+        public double Area()
+        {
+                double area = side * side;
+            return area;
+        }
+        public bool Legitimate()
+        {
+            if (side > 0)
+                return legitimate = true;
+            else
+                return legitimate = false;
+        }
+    }
+    class Triangle : Shape
+    {
+        public double a, b, c;
+        private bool legitimate;
+
+        public Triangle(double x, double y, double z)
+        {
+            a = x;
+            b = y;
+            c = z;
+        }
+        public bool Legitimate()
+        {
+            if ((a - b) <= c)
+                return legitimate = false;
+            else
+                return legitimate = true;
+        }
+        public double Area()
+        {
+                double p = (a + b + c) / 2;
+                double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+    }
+    class ShapeFactory
+    {
+        public static Random random()
+        {
+            Random R = new Random(Guid.NewGuid().GetHashCode());
+            return R;
+        }
+        public static Shape getShape(int n)
+        {
+            Shape shape = null;
+            if (n==0)
+            {
+                Random rd1 = new Random();
+                int x,y;
+                x = rd1.Next(1,10);
+                y = rd1.Next(1,10);
+                shape = new Rectangle(x,y);
+            }
+            else if (n==1)
+            {
+                Random rd2 = new Random();
+                int side = rd2.Next(1, 10);
+                shape = new Square(side);
+            }
+            else if (n==2)
+            {
+                Random rd3 = new Random();
+                int a, b, c;
+                a = rd3.Next(1, 10);
+                b = rd3.Next(1, 10);
+                c = rd3.Next(1, 10);
+                shape = new Triangle(a,b,c);
+            }
+            return shape;
+        }
+    }
+    class Program
+    {
+        public static Random random()
+        {
+            Random R = new Random(Guid.NewGuid().GetHashCode());
+            return R;
+        }
+        static void Main(string[] args)
+        {
+            Shape shape;
+            double areas = 0;
+            for(int i = 0;i < 10;i++)
+            {
+                shape = ShapeFactory.getShape(ShapeFactory.random().Next(0,2));
+                if (shape.Legitimate())
+                {
+                    areas += shape.Area();
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            Console.WriteLine(areas);
+            Console.ReadLine();
+        }
+    }
+}
