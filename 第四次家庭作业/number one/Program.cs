86dd40cc57d46c6cc5f7896d010d0c267003e4c5
ysx1024 <2650974406@qq.com>
2020-03-14 14:23:43 +0800

    -m

---
+++ 第四次家庭作业/number one/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace number_one
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = this.head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x=0;x<10;x++)
+            {
+                intlist.Add(x);
+            }
+            intlist.ForEach(x=>Console.WriteLine(x));
+            int max = 0;
+            int min = 0;
+            int sum = 0;
+
+            intlist.ForEach(x=> 
+            {
+                if (max < x)
+                    max = x;
+            });
+            Console.WriteLine("最大值为："+max);
+
+            intlist.ForEach(x=>
+            {
+                if (min > x)
+                    min = x;
+            });
+            Console.WriteLine("最小值为："+min);
+
+            intlist.ForEach(x=> sum+=x);
+            Console.WriteLine("和为："+sum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 第四次家庭作业/number two/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace number_two
+{
+    public delegate void AlarmTick(Object a, Alarm t);
+    public delegate void AlarmGo(Object a, Alarm t);
+
+    public class Alarm
+    {
+        public int S, M, H;
+
+        public Alarm(int a,int b,int c)
+        {
+            S = a;
+            M = b;
+            H = c;
+        }
+        public event AlarmTick Tick;
+        public event AlarmGo alar;
+
+        public void Alarmtick()
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                S++;
+                if (S == 61)
+                {
+                    M++;
+                    S = 0;
+                }
+                if (M == 61)
+                {
+                    H++;
+                    M = 0;
+                }
+                if (H == 25)
+                {
+                    H = 1;
+                }
+                Tick(null, this);
+                alar(null, this);
+            }    
+        }
+    }
+    class Program
+    {
+        static void tick(object a,Alarm t)
+        {
+            Action<int> action = s => Console.WriteLine(s);
+            action(t.S);
+            action(t.M);
+            action(t.H);
+        }
+        static void alarm(object a, Alarm t)
+        {
+            if (t.S == 12 && t.M == 19 && t.H == 16)
+                Console.WriteLine("响铃");
+        }
+        static void Main(string[] args)
+        {
+            Alarm a = new Alarm(50,18,16);
+            a.Tick += tick;
+            a.alar += alarm;
+            a.Alarmtick();
+
+            Console.ReadLine();
+        }
+    }
+}
